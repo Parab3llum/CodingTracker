df6680eda76e371816a541dd7e7d328e1bfea48f
parabeiium <vladberlin65@gmail.com>
2023-05-19 11:54:25 +0300

    Started Project Math Game

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -10,22 +10,8 @@ namespace CodingTracker
     {
         static void Main(string[] args)
         {
-            Medhod1();
-            Medhod2();
-            Medhod3();
+            
 
-        }
-        public static void Medhod1()
-        {
-            Console.WriteLine("This");
-        }
-        public static void Medhod2()
-        {
-            Console.WriteLine("is");
-        }
-        public static void Medhod3()
-        {
-            Console.WriteLine("a new Branch");
         }
     }
 }
