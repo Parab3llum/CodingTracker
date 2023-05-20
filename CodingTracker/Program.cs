951d68fdfe2253d85c9fb20ed113a4108632d4e3
parab3iium <vladberlin65@gmail.com>
2023-05-20 15:04:24 +0300

    Added function to all operators and showing history

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Runtime.CompilerServices;
@@ -9,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace CodingTracker
 {
-    internal class Program
+    internal partial class Program
     {
         static void Main(string[] args)
         {
@@ -17,116 +16,6 @@ namespace CodingTracker
             Console.WriteLine("Welcome To the Math Game , Where you choose type of Game and answer questions accordingly");
             var game = new Runner();
             game.Run();
-        }
-        public class Runner
-        {
-            private bool running = true;
-            private string userChoice;
-            List<string> history = new List<string>();
-            Random rn= new Random();
-            public void Run()
-            {
-                while (running == true)
-                {
-                    Console.WriteLine("Choose your type of game \n" +
-                        "A - Addition\n" +
-                        "S - Subtraction\n" +
-                        "M - Multiplication\n" +
-                        "D - Division");
-                    userPickChoice();
-                }
-            }
-            public void userPickChoice()
-            {
-                userChoice = Console.ReadLine().ToLower();
-                if (userChoice == "a" || userChoice == "s" || userChoice == "m" || userChoice == "d")
-                    switch (userChoice)
-                    {
-                        case "a":
-                            Console.Clear();
-                            running= false;
-                            Console.WriteLine("add");
-                            OperationMode("a");
-                            break;
-                        case "s":
-                            Console.Clear();
-                            running= false;
-                            Console.WriteLine("sub");
-                            break;
-                        case "m":
-                            Console.Clear();
-                            running= false;
-                            Console.WriteLine("mul");
-                            break;
-                        case "d":
-                            Console.Clear();
-                            running= false;
-                            Console.WriteLine("div");
-
-                            break;
-                    }
-                else
-                {
-                    {
-                        Console.Clear();
-                        Console.WriteLine("invalid Choice");
-                        Console.WriteLine();
-                    }
-                }
-            }
-            public void OperationMode(string oper)
-            {
-                int firstNum = rn.Next(0, 100);
-                int secondNum = rn.Next(0, 100);
-                int sum = firstNum+ secondNum;
-                int userAnswer;
-                switch (oper)
-                {
-                    case "a":
-                        Check(firstNum, secondNum, sum , "+");
-                        break;
-                }
-                
-            }
-
-            public void Check(int firstNum, int secondNum, int sum, string oper)
-            {
-                string currentQuestion = $"{ firstNum } {oper} { secondNum} = ? ";
-                int userAnswer;
-                bool answer;
-                Console.WriteLine(currentQuestion);
-                int.TryParse(Console.ReadLine(), out userAnswer);
-                if (userAnswer != sum)
-                {
-                    answer = false;
-                    Console.WriteLine("Wrong answer");
-                }
-                else
-                {
-                    answer = true;
-                    Console.WriteLine("Correct");
-
-                }
-                currentQuestion += (userAnswer.ToString() +" "+ answer.ToString());
-                history.Add(currentQuestion);
-                running= true;
-            }
-        }
-        public class Addition
-        {
-
-        }
-        public class Subtraction
-        {
-
-        }
-        public class Multiplication
-        {
-
-        }
-        public class Divison
-        {
-
         }
     }
 }
---
+++ CodingTracker/Runner.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodingTracker
+{
+    internal partial class Program
+    {
+        public class Runner
+        {
+            private bool running = true;
+            private string userChoice;
+            List<string> history = new List<string>();
+            Random rn = new Random();
+            public void Run()
+            {
+                while (running == true)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Choose your type of game \n" +
+                        "A - Addition\n" +
+                        "S - Subtraction\n" +
+                        "M - Multiplication\n" +
+                        "D - Division\n" +
+                        "H - Show History");
+                    userPickChoice();
+                }
+            }
+            public void userPickChoice()
+            {
+                userChoice = Console.ReadLine().ToLower();
+                if (userChoice == "a" || userChoice == "s" || userChoice == "m" || userChoice == "d")
+                {
+                    OperationMode(userChoice);
+                }
+                else if (userChoice == "h")
+                {
+                    if (history != null)
+                    {
+
+                        foreach (var item in history)
+                        {
+                            Console.WriteLine(item);
+                        }
+                    }
+                }
+
+                else
+                {
+                    {
+                        Console.Clear();
+                        Console.ForegroundColor= ConsoleColor.Red;
+                        Console.WriteLine("invalid Choice");
+                        Console.WriteLine();
+                    }
+                }
+            }
+            public void OperationMode(string oper)
+            {
+                Console.Clear();
+                int firstNum = rn.Next(0, 100);
+                int secondNum = rn.Next(0, 100);
+                int sum = 0;
+                //switch (oper)
+                //{
+                //    case "+":
+                //        sum = firstNum + secondNum;
+                //        break;
+                //    case "-":
+                //        sum = firstNum - secondNum;
+                //        break;
+                //    case "*":
+                //        sum = firstNum * secondNum;
+                //        break;
+                //    case "/":
+                //        sum = firstNum / secondNum;
+                //        break;
+
+                //    default:
+                //        break;
+                //}
+                int userAnswer;
+                switch (oper)
+                {
+                    case "a":
+                        Check(firstNum, secondNum, sum, "+");
+                        break;
+                    case "s":
+                        Check(firstNum, secondNum, sum, "-");
+                        break;
+                    case "m":
+                        Check(firstNum, secondNum, sum, "*");
+                        break;
+                    case "d":
+                        Check(firstNum, secondNum, sum, "/");
+                        break;
+                }
+
+
+            }
+
+            public void Check(int firstNum, int secondNum, int sum, string oper)
+            {
+                string currentQuestion = $"{firstNum} {oper} {secondNum} = ? ";
+                int userAnswer;
+                bool answer;
+                switch (oper)
+                {
+                    case "+":
+                        sum = firstNum + secondNum;
+                        break;
+                    case "-":
+                        sum = firstNum - secondNum;
+                        break;
+                    case "*":
+                        sum = firstNum * secondNum;
+                        break;
+                    case "/":
+                        sum = firstNum / secondNum;
+                        break;
+
+                    default:
+                        break;
+                }
+                Console.WriteLine(currentQuestion);
+                int.TryParse(Console.ReadLine(), out userAnswer);
+                if (userAnswer != sum)
+                {
+                    answer = false;
+                    Console.ForegroundColor= ConsoleColor.Red;
+                    Console.WriteLine("Wrong answer");
+                }
+                else
+                {
+                    answer = true;
+                    Console.ForegroundColor= ConsoleColor.Green;
+                    Console.WriteLine("Correct");
+
+                }
+                currentQuestion += (userAnswer.ToString() + " " + answer.ToString());
+                history.Add(currentQuestion);
+                running = true;
+            }
+        }
+    }
+}
